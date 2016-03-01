cdeb189703e5f645d67942d5552085a3a496492c
divyavarshini <divya.varshini11@gmail.com>
2016-03-01 13:04:00 -0800

    Database Connection

---
+++ src/main/java/org/divya/mitchell/database/DatabaseConnection.java
@@ -0,0 +1,33 @@
+package org.divya.mitchell.database;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String URL = "jdbc:mysql://localhost:3306/mitchellclaim";
+	
+	static final String USER = "root";
+	static final String PSWD = "Gulabo@123";
+	
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		try{
+			Class.forName(JDBC_DRIVER).newInstance();
+			conn = DriverManager.getConnection(URL, USER, PSWD);
+		}catch(SQLException e){
+			e.printStackTrace();
+		} catch (InstantiationException ie) {
+			ie.printStackTrace();
+		} catch (IllegalAccessException il) {
+			il.printStackTrace();
+		} catch (ClassNotFoundException ce) {
+			ce.printStackTrace();
+		}
+		return conn;
+	}
+	
+	
+	
+}
